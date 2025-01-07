584bc916e0257d5e6df9aa4120623af2ab428f6f
pra_ssan_na <prasanavenkatesh2003@gmail.com>
2025-01-07 17:29:30 +0530

    new added files with variables

---
+++ variables/Variables types.php
@@ -0,0 +1,33 @@
+<?php
+
+// varaiable type for the integer
+$x = 5;
+var_dump($x);
+echo "<br>";
+
+// varaiable type for the float
+$x = 10.365;
+var_dump($x);
+echo "<br>";
+
+// varaiable type for the string
+$x = "I am a string";
+var_dump($x);
+echo "<br>";
+
+// varaiable type for the boolean
+$x = true;
+var_dump($x);
+echo "<br>";
+
+// varaiable type for the array
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);   
+echo "<br>";
+
+// varaiable type for the NULL
+$x = null;
+var_dump($x);
+
+
+?>
--- variables/varaiable.php
+++ variables/varaiable.php
@@ -5,7 +5,13 @@ echo $x;
 echo "<br>";
 echo $y;
 echo "<br>";   
-
+// Adding the two variable
 $txt = "This is the example of string";
 echo "Varaible : $txt";
+
+// Adding the two numbers
+$x = 5;
+$y = 10;
+echo "<br>";
+echo $x + $y;
 ?>
