fecfe05478583d34ba14a734a915cb1d2cdb3d3b
pra_ssan_na <prasanavenkatesh2003@gmail.com>
2025-01-08 15:59:41 +0530

    newly added type of variable

--- variables/variable scope.php
+++ variables/variable scope.php
@@ -48,8 +48,24 @@ function myTest4() {
    $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
 }
 myTest4();
-echo $y; // outputs 36
+echo $y, "<br>"; // outputs 36
 
 
+// static keyword
+// when a function is completed/executed, all of its variables are deleted. However, sometimes we want a local variable not to be deleted. We need it for a further job.
+// To do this, use the static keyword when you first declare the variable:
+function myTest5() {
+    static $x = 0;
+    echo $x;
+    $x++;
+}
+myTest5();echo "<br>";
+myTest5();echo "<br>";
+myTest5();echo "<br>";
+myTest5();echo "<br>";
+myTest5();
+// Then, each time the function is called, that variable will still have the information it contained from the last time the function was called.   
+// The variable is still local to the function.
+// The static keyword can be used to update a local variable.
 
 ?>
